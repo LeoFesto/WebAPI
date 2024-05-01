46ec4d3d203081695f1c119a9105bf02a47c80a1
Bogdan <leofesto1523@gmail.com>
2024-05-01 17:01:35 +0300

    Init

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet("FirstMethod")]
+        public IActionResult FirstTest()
+        {
+            return Ok("first");
+        }
+
+        [HttpGet("SecondMethod")]
+        public IActionResult SecondTest()
+        {
+            return Ok("second");
+        }
+    }
+}
